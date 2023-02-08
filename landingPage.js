f65f7204d782da6a59367a8710d6fc4689cb2b62
marc hudson <marchudson2601@gmail.com>
2023-02-08 18:41:27 +0000

    added javascript to norm the data from the random meal API

--- landingPage.js
+++ landingPage.js
@@ -5,6 +5,56 @@ $('.dropdown').on('hide.bs.dropdown', function () {
     document.querySelector(".overlay").classList.add("d-none")
 })
 
+//
+
+// fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+//     .then(response => response.json())
+//     .then(response => {
+//         if (!response.meals) {
+//             // if there is no data throw an error
+//             throw "No Data";
+//         }
+//         const meal = response.meals[0]
+//         const ingredients = []
+//         const measurements = []
+
+//         for (const key in meal) {
+//             //because the api response doesn't  contain an array of ingredients we loop thought the keys to create an array
+//             if (key.startsWith("strIngredient")) {
+//                 if (meal[key] !== "") {
+//                     ingredients.push(meal[key].trim())
+//                 }
+//             }
+//             if (key.startsWith("strMeasure")) {
+//                 if (meal[key] !== "") {
+//                     measurements.push(meal[key].trim())
+//                 }
+//             }
+//         }
+//         const recipeIngredents = []
+//         for (let i = 0; i < ingredients.length; i++) {
+//             const ingredent = ingredients[i];
+//             const measurement = measurements[i]
+//             recipeIngredents.push(`${measurement} ${ingredent}`)
+//         }
+//         //for the meal name
+//         console.log(response.meals[0].strMeal)
+//         //this gets the ingredents
+//         console.log(recipeIngredents)
+//         //cooking instructions
+//         console.log(response.meals[0].strInstructions)
+//         //for the the thumbnail image
+//         console.log(response.meals[0].strMealThumb)
+//         // for the u tube video link
+//         console.log(response.meals[0].strYoutube)
+//         console.log(ingredients)
+//     })
+
+//     .catch(() => {
+//         // if there is an error render some error message
+//         console.log("There was an error")
+//     })
+
 
 // let userInput = "sushi"
 
