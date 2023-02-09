679c919d0206528bd70aa49fb104d504cf331bf4
marc hudson <marchudson2601@gmail.com>
2023-02-09 01:50:51 +0000

    added map feature added to eat out page general changes to js and html

--- js/eatin.js
+++ js/eatin.js
@@ -1,7 +1,10 @@
 const randomButton = document.getElementById("random-meal");
 
+
+
 randomButton.addEventListener("click", function () {
     generateCardRandom()
+    generateCardSelected()
 });
 
 renderHistory();
@@ -40,6 +43,9 @@ function renderHistory() {
 
 function renderMeal(data) {
     renderHistory()
+
+    const cookingInstructions = data.cookingInstructions.split("\r\n").filter(x => x);
+
     document.querySelector(".meal-container").innerHTML = ` 
         <div class="meal-eatin">
             <h5 class="card-title">${data.mealName}</h5>
@@ -81,7 +87,11 @@ function renderMeal(data) {
                     <div class="modal-dialog modal-lg">
                         <div class="modal-content">
                             <h3>Cooking instructions</h3>
-                            <p>${data.cookingInstructions}</p>
+                            <ol>
+                                ${cookingInstructions.map(step => `
+                                    <li>${step}</li>
+                                `).join("")}
+                            </ol>
                         </div>
                     </div>
                 </div>
@@ -182,8 +192,157 @@ function getRandomMeal() {
 
 
 
+///////////////////////////////////////////////////////////////////////////////////
 
-// let userInput = "toad in the hole"
+
+
+// function generateCardSelected() {
+//     getRandomMeal().then(
+//         data => {
+//             saveHistory({
+//                 id: data.id,
+//                 mealName: data.mealName
+//             })
+//             getSelectedMeal(data)
+//         }
+//     )
+// }
+
+// function getSelectedMeal() {
+//     userInput = "Eton Mess"
+//     // this gets the ingredients
+//     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`)
+//         .then(response => response.json())
+//         .then(toSelectMealOjt)
+//         .catch(() => {
+//             // if there is an error render some error message
+//             console.log("There was an error")
+//         })
+
+// }
+
+// function toSelectMealOjt() {
+//     // this gets the ingredients
+//     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`)
+//         .then(response => response.json())
+//         .then(response => {
+
+//             if (!response.meals) {
+//                 // if there is no data throw an error
+//                 throw "No Data";
+//             }
+//             const meal = response.meals[0]
+//             const ingredients = []
+//             const measurements = []
+
+//             for (const key in meal) {
+//                 //because the api response doesn't  contain an array of ingredients we loop thought the keys to create an array
+//                 if (key.startsWith("strIngredient")) {
+//                     if (meal[key] !== "") {
+//                         ingredients.push(meal[key].trim())
+//                     }
+//                 }
+//                 if (key.startsWith("strMeasure")) {
+//                     if (meal[key] !== "") {
+//                         measurements.push(meal[key].trim())
+//                     }
+//                 }
+//             }
+//             const recipeIngredents = []
+//             for (let i = 0; i < ingredients.length; i++) {
+//                 const ingredent = ingredients[i];
+//                 const measurement = measurements[i]
+//                 recipeIngredents.push(`${measurement} ${ingredent}`)
+//             }
+//             //for the meal name
+//             const sMealName = response.meals[0].strMeal
+//             //this gets the ingredents
+//             const sIngredents = recipeIngredents
+//             //cooking instructions
+//             const sCookingInstructions = response.meals[0].strInstructions
+//             //for the the thumbnail image
+//             const sImage = response.meals[0].strMealThumb
+//             // for the u tube video link
+//             const sVidioLink = response.meals[0].strYoutube
+//             return {
+//                 id: meal.idMeal,
+//                 sMealName,
+//                 sIngredents,
+//                 sCookingInstructions,
+//                 sImage,
+//                 sVidioLink,
+//             }
+//         })
+
+// }
+
+// function getSelectedMeal(data) {
+//     renderHistory()
+//     document.querySelector(".meal-container").innerHTML = ` 
+//         <div class="meal-eatin">
+//             <h5 class="card-title">${data.sMealName}</h5>
+//             <img class="card-img-top" src="${data.sImage}" alt="Card image cap">
+//             <div class="card-body">
+//                 <ul>
+//                     ${data.ingredents.map(i => `<li>${i}</li>`).join("")}
+//                 </ul>
+
+//                 <button
+//                     type="button"
+//                     class="btn btn-primary"
+//                     data-toggle="modal"
+//                     data-target=".dialog-video"
+//                 >
+//                     Video
+//                 </button>
+        
+//                 <button
+//                     type="button"
+//                     class="btn btn-primary"
+//                     data-toggle="modal"
+//                     data-target=".dialog-cooking-instructions"
+//                 >
+//                     Cooking Instructions
+//                 </button>
+        
+//                 <div class="modal fade dialog-video" tabindex="-1" role="dialog" aria-hidden="true">
+//                     <div class="modal-dialog modal-lg">
+//                         <div class="modal-content">
+//                             <h3>Video</h3>
+//                             <div class="video-container">
+//                                 <iframe width="640" height="385" src="${data.sVidioLink?.replace("/watch?v=", "/embed/")}?autoplay=1&mute=1"></iframe>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div class="modal fade dialog-cooking-instructions" tabindex="-1" role="dialog" aria-hidden="true">
+//                     <div class="modal-dialog modal-lg">
+//                         <div class="modal-content">
+//                             <h3>Cooking instructions</h3>
+//                             <p>${data.sCookingInstructions}</p>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     `
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let userInput =
 
 // // this gets the ingredients
 // fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`)
@@ -234,32 +393,3 @@ function getRandomMeal() {
 //     .catch(() => {
 //         alert("no meal")
 //     })
-      // const options = {
-        //     method: 'GET',
-        //     headers: {
-        //         'X-RapidAPI-Key': 'bda18aceb6msh4cfb8ec1d5bdafep1699e7jsnf33daa16b16d',
-        //         'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
-        //     }
-        // };
-        // //fetch request with loop to pull put the ingredients and push into the ingredients array
-        // fetch(`https://edamam-recipe-search.p.rapidapi.com/search?q=${userInput}`, options)
-        //     .then(response => response.json())
-        //     .then(response => {
-        //         console.log(response)
-        //         ingredients = []
-        //         const path = response.hits[3].recipe.ingredientLines
-        //         for (let i = 0; i < path.length; i++) {
-        //             ingredients.push(path[i]);
-        //         }
-
-        //         // // meal name
-        //         // console.log(response.q)
-        //         // // ingredients
-        //         // console.log(ingredients)
-        //         // // calorie content
-        //         // console.log(`calories ${Math.trunc(response.hits[3].recipe.calories)}`)
-        //         // //image
-        //         // console.log(response.hits[3].recipe.image)
-        //         // //link to cooking istructions
-        //         // console.log(response.hits[3].recipe.url)
-        //     })
--- js/eatout.js
+++ js/eatout.js
@@ -1,21 +1,163 @@
-// const userInputt = "clowne"
+const apiKey = `85ab5ccbe5924069b86a34a443887846`
 
-// const apiKey = `85ab5ccbe5924069b86a34a443887846`
-// //
-// function getLocation(place) {
-//     return fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${place}&type=city&format=json&apiKey=${apiKey}`)
-//         .then(response => response.json())
-//         .then(result => result.results[0].place_id);
+const button = document.getElementById("buttonSearch");
+
+document.getElementById("location").addEventListener("input", (e) => {
+    if (e.target.value === "") {
+        button.classList.add("disabled")
+        return;
+    }
+    button.classList.remove("disabled");
+});
+
+function setLoading(isLoading) {
+    const loadingSpinner = document.querySelector("#buttonSearch .loading")
+    if (isLoading) {
+        loadingSpinner.classList.remove("d-none");
+        button.classList.add("disabled")
+        return
+    }
+    loadingSpinner.classList.add("d-none");
+    button.classList.remove("disabled")
+}
+
+function saveCache(places) {
+    localStorage.setItem("location-history", JSON.stringify(places));
+}
+
+function getCache() {
+    if (localStorage.getItem("location-history") === null) {
+        return [];
+    }
+    return JSON.parse(localStorage.getItem("location-history"));
+}
+
+
+// {
+//     "name": "Brocco On The Park",
+//     "country": "United Kingdom",
+//     "country_code": "gb",
+//     "state": "England",
+//     "county": "South Yorkshire Mayoral Combined Authority",
+//     "city": "Sheffield",
+//     "postcode": "S11 8RS",
+//     "district": "Broomfield",
+//     "suburb": "Sharrow Vale",
+//     "street": "Brocco Bank",
+//     "housenumber": "92",
+//     "lon": -1.5021718858522726,
+//     "lat": 53.3681689,
+//     "state_code": "ENG",
+//     "formatted": "Brocco On The Park, 92 Brocco Bank, Sheffield S11 8RS, United Kingdom",
+//     "address_line1": "Brocco On The Park",
+//     "address_line2": "92 Brocco Bank, Sheffield S11 8RS, United Kingdom",
+//     "categories": [
+//         "accommodation",
+//         "accommodation.hotel",
+//         "building",
+//         "building.accommodation",
+//         "building.catering",
+//         "catering",
+//         "catering.restaurant"
+//     ],
+//     "details": [
+//         "details",
+//         "details.contact"
+//     ],
+//     "datasource": {
+//         "sourcename": "openstreetmap",
+//         "attribution": "© OpenStreetMap contributors",
+//         "license": "Open Database Licence",
+//         "url": "https://www.openstreetmap.org/copyright",
+//         "raw": {
+//             "name": "Brocco On The Park",
+//             "email": "hello@brocco.co.uk",
+//             "phone": "+44 114 266 1233",
+//             "osm_id": 99515829,
+//             "amenity": "restaurant",
+//             "fhrs:id": 838970,
+//             "tourism": "hotel",
+//             "website": "https://www.brocco.co.uk/",
+//             "building": "yes",
+//             "osm_type": "w",
+//             "addr:city": "Sheffield",
+//             "addr:street": "Brocco Bank",
+//             "addr:postcode": "S11 8RS",
+//             "opening_hours": "Mo-Sa 08:30-22:00; Su 08:30-18:00",
+//             "addr:housenumber": 92
+//         }
+//     },
+//     "place_id": "517497ee98dd08f8bf59e029fe6420af4a40f00102f901b57dee050000000092031242726f63636f204f6e20546865205061726b"
 // }
 
-// function getRestaurants(placeId) {
-//     return fetch(`https://api.geoapify.com/v2/places?categories=catering.restaurant&filter=place:${placeId}&limit=20&apiKey=${apiKey}`)
-//         .then(response => response.json())
-//         .then(result => result)
-// }
+function getSpecificPlace(e) {
+    const { id } = e.dataset;
 
-// getLocation(userInputt).then(placeId => {
-//     getRestaurants(placeId).then(restaurants => {
-//         console.log(restaurants.features.map(x => x.properties.name).filter(x => x))
-//     })
-// })
+    const width = 400;
+    const height = 300;
+
+    const place = getCache().find(history => history.properties.place_id === id).properties
+    const lon = place.lon
+    const lat = place.lat
+
+    let website = "";
+    if (place.datasource.raw.website) {
+        website = `<p><a target="_blank" href="${place.datasource.raw.website}">Website</a></p>`
+    }
+
+    let openingHours = ""
+    if (place.datasource.raw.opening_hours) {
+        openingHours = `<p> Opening hours ${place.datasource.raw.opening_hours}</p>`
+    }
+
+    let phone = "";
+    if (place.datasource.raw.phone) {
+        website = `<p>Contact Number: <a href="tel:${place.datasource.raw.phone}">${place.datasource.raw.phone}</a></p>`
+    }
+
+    document.getElementById("selected-restaurant").innerHTML = `
+        <h1>${place.name}</h1>
+        ${website}
+        <p> ${place.address_line2}</p>
+        ${openingHours}
+        ${phone}
+        <img width="${width}" height="${height}" class="map-image" src="https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=${width}&height=${height}&center=lonlat:${lon},${lat}&zoom=13&marker=lonlat:${lon},${lat};type:material;color:%23ff3421;icontype:awesome|lonlat:${lon},${lat};type:material;color:%23ff3421;icontype:awesome&apiKey=${apiKey}"></img>
+    `
+}
+
+// https://maps.geoapify.com/v1/staticmap?style=osm-bright-smooth&width=600&height=400&center=lonlat%3A-122.29009844646316%2C47.54607447032754&zoom=14.3497&marker=lonlat%3A-122.29188334609739%2C47.54403990655936%3Btype%3Aawesome%3Bcolor%3A%23bb3f73%3Bsize%3Ax-large%3Bicon%3Apaw%7Clonlat%3A-122.29282631194182%2C47.549609195001494%3Btype%3Amaterial%3Bcolor%3A%234c905a%3Bicon%3Atree%3Bicontype%3Aawesome%7Clonlat%3A-122.28726954893025%2C47.541766557545884%3Btype%3Amaterial%3Bcolor%3A%234c905a%3Bicon%3Atree%3Bicontype%3Aawesome&apiKey=85ab5ccbe5924069b86a34a443887846
+
+button.addEventListener("click", e => {
+    e.preventDefault();
+
+    const locationSelecton = document.getElementById("location").value;
+    setLoading(true)
+    getLocation(locationSelecton).then(placeId => {
+        getRestaurants(placeId).then(restaurants => {
+            const places = restaurants.features.filter(x => x.properties.name)
+            saveCache(places);
+
+            document.getElementById("input-locations").innerHTML = `
+                ${places.map(place => `<button data-id="${place.properties.place_id}" onclick="getSpecificPlace(this)">${place.properties.name}</button>`).join("")}
+            `
+
+            setLoading(false)
+        })
+    }).catch(() => {
+        setLoading(false)
+    })
+})
+
+
+
+function getLocation(place) {
+    return fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${place}&type=city&format=json&apiKey=${apiKey}`)
+        .then(response => response.json())
+        .then(result => result.results[0].place_id);
+}
+
+function getRestaurants(placeId) {
+    return fetch(`https://api.geoapify.com/v2/places?categories=catering.restaurant&filter=place:${placeId}&limit=20&apiKey=${apiKey}`)
+        .then(response => response.json())
+        .then(result => result)
+}
